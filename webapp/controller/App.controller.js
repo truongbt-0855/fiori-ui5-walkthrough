5b9b42f9ef45f13cc981a6949ddfeddfafdc60f6
truongbt <bui.tuan.truong@sun-asterisk.com>
2025-04-01 16:44:37 +0700

    Step 8: Translate Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,24 +2,35 @@ sap.ui.define([
   'sap/ui/core/mvc/Controller',
   'sap/m/MessageToast',
   'sap/ui/model/json/JSONModel',
-], (Controller, MessageToast, JSONModel) => {
+  'sap/ui/model/resource/ResourceModel'
+], (Controller, MessageToast, JSONModel, ResourceModel) => {
   'use strict';
 
   return Controller.extend('ui5.walkthrough.controller.App', {
-    onInit () {
+    onInit() {
       // set data model on view
       const oData = {
         recipient: {
-          name: 'World',
+          name: 'World'
         }
-      }
+      };
 
       const oModel = new JSONModel(oData);
       this.getView().setModel(oModel);
+
+      // set i18n model on view
+      const i18nModel = new ResourceModel({
+        bundleName: 'ui5.walkthrough.i18n.i18n',
+      });
+      this.getView().setModel(i18nModel, 'model_i18n');
     },
     onShowHello() {
-      // alert('Hello World!');
-      MessageToast.show('Hello World!');
+      // read msg from i18n model
+      const oBundle = this.getView().getModel('model_i18n').getResourceBundle();
+      const sRecipient = this.getView().getModel().getProperty('/recipient/name');
+      const sMsg = oBundle.getText('helloMsg', [sRecipient]);
+
+      MessageToast.show(sMsg);
     }
   });
 });
