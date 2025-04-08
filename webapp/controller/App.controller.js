5a2ba2900175af58ae5a250f00c95f00cd34e596
truongbt <bui.tuan.truong@sun-asterisk.com>
2025-04-08 10:31:29 +0700

    Step 14: Custom CSS Step 15: Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,13 +5,6 @@ sap.ui.define([
   'use strict';
 
   return Controller.extend('ui5.walkthrough.controller.App', {
-    onShowHello() {
-      // read msg from i18n model
-      const oBundle = this.getView().getModel('model_i18n').getResourceBundle();
-      const sRecipient = this.getView().getModel().getProperty('/recipient/name');
-      const sMsg = oBundle.getText('helloMsg', [sRecipient]);
 
-      MessageToast.show(sMsg);
-    }
   });
 });
